549fa290c924b2d6b3f76658704a68cd6b6d8655
tntman5000 <tntman5000@gmail.com>
2015-02-08 16:31:03 -0700

    moving things!

--- Bens_Game/GameEngine/firstScreen/FirstScreen.java
+++ Bens_Game/GameEngine/firstScreen/FirstScreen.java
@@ -8,15 +8,19 @@ import main.Screen;
 
 public class FirstScreen implements Screen {
 
+	private int x = 0;
+	private int y = 40;
+	
 	public void update() {
 		// TODO Auto-generated method stub
 		System.out.println("the screen works");
+		x++;
 	}
 
 	public void render(Graphics g) {
 		// TODO Auto-generated method stub
 		g.setColor(Color.CYAN);
-		g.fillRect(69, 80, 100, 40);
+		g.fillRect(x, y, 100, 40);
 
 	}
 
